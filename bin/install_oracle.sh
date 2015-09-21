9ecf59e86cec8228d2fc37c2108ef5cc62a5ac7e
Mark Fischer <fischerm@email.arizona.edu>
2015-09-21 11:15:11 -0700

    UAF-433 Adding Oracle SQLPlus tools

---
+++ bin/install_oracle.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# This script copies a bunch of files from /oracle_installers into various 
+# places required to run the sqlplus command line tool for Oracle DB access.
+#
+if [ ! -d $ORA_BASE_DIR ] 
+then
+    mkdir $ORA_BASE_DIR
+fi
+
+unzip $ORA_SOURCE_DIR/instantclient-basic-linux.x64*.zip -d $ORA_BASE_DIR
+unzip $ORA_SOURCE_DIR/instantclient-sqlplus-linux.x64*.zip -d $ORA_BASE_DIR
+
+echo "$ORA_DIR/" > /etc/ld.so.conf.d/oracle.conf
+ldconfig
+
